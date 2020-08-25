9c5f5a04739b783ed625e26b18d6d0df9a845fd3
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2020-08-25 11:21:42 +0200

    Update Class1.cs

--- Signal.Infrastructure.AzureDevOps/Class1.cs
+++ Signal.Infrastructure.AzureDevOps/Class1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Commerce.Client;
 using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.ServiceHooks.WebApi;
 using Microsoft.VisualStudio.Services.WebApi;
 
 namespace Signal.Infrastructure.AzureDevOps
@@ -24,6 +26,23 @@ namespace Signal.Infrastructure.AzureDevOps
         {
             var projects = await GetContext(azureDevOpsOrganizationUrl, dfnoiseDevOpsPatFull).Connection.GetClient<ProjectHttpClient>().GetProjects();
             return projects.Select(p => new DevOpsProject {Id = p.Id, Name = p.Name});
+        }
+
+        public static async Task CreateSubscriptionAsync()
+        {
+            var context = GetContext(azureDevOpsOrganizationUrl, dfnoiseDevOpsPatFull);
+            var project = await context.Connection
+                .GetClient<ProjectHttpClient>().GetProject("5efbd867-e379-42aa-b5f4-801b5141f4af");
+            var hooks = context.Connection.GetClient<ServiceHooksManagementHttpClient>();
+            await hooks.CreateSubscriptionAsync(new Subscription()
+            {
+                PublisherId = "tfs",
+                EventType = "build.complete",
+                ConsumerId = "azureStorageQueue",
+                ConsumerActionId = "enqueue",
+                ActionDescription = "Account signalstorageaccount, Queue azuredevops"
+            });
+
         }
 
         private static DevOpsContext GetContext(string organizationUrl, string pat)
