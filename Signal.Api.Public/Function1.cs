1972a0d736299a6909929685c16b2056b9c458d2
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2020-12-04 00:25:53 +0100

    Updating API

---
+++ Signal.Api.Public/Function1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Signal.Api.Public
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            var creds = string.Empty;
+            if (req.Headers.TryGetValue("x-ms-client-principal", out var principal))
+                if (principal.Any())
+                    creds = principal[0];
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. Your credentials: {creds}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ Signal.Core/Beacon/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Signal.Core.Beacon
+{
+    public interface IBeaconService
+    {
+        Task RegisterAsync(string email, CancellationToken cancellationToken);
+    }
+
+    internal class BeaconService : IBeaconService
+    {
+        private readonly ILogger<BeaconService> logger;
+
+
+        public BeaconService(ILogger<BeaconService> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        public async Task RegisterAsync(string email, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // TODO: Add to beacons table
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, "Failed to register beacon to email {BeaconRegisterEmail}.", email);
+            }
+        }
+    }
+
+    public interface IBeaconRegistrationTableEntity : ITableEntity
+    {
+        DateTime? RequestDateTime { get; set; }
+    }
+
+    public interface IBeaconTableEntity : ITableEntity
+    {
+    }
+
+    public interface ITableEntity
+    {
+        string PartitionKey { get; set; }
+
+        string RowKey { get; set; }
+    }
+}
