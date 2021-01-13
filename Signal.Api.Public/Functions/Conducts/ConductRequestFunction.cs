7d77d62dfa066ac9d6cd1f6588822a017ab27a73
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-01-14 00:21:50 +0100

    Added conduct request function

---
+++ Signal.Api.Public/Functions/Conducts/ConductRequestFunction.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Signal.Api.Public.Auth;
+using Signal.Api.Public.Exceptions;
+using Signal.Core;
+
+namespace Signal.Api.Public.Functions.Conducts
+{
+    public class ConductRequestFunction
+    {
+        private readonly IFunctionAuthenticator functionAuthenticator;
+        private readonly IAzureStorage storage;
+        private readonly IAzureStorageDao storageDao;
+
+        public ConductRequestFunction(
+            IFunctionAuthenticator functionAuthenticator,
+            IAzureStorage storage,
+            IAzureStorageDao storageDao)
+        {
+            this.functionAuthenticator = functionAuthenticator ?? throw new ArgumentNullException(nameof(functionAuthenticator));
+            this.storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            this.storageDao = storageDao ?? throw new ArgumentNullException(nameof(storageDao));
+        }
+
+        [FunctionName("Devices-Register")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "devices/register")]
+            HttpRequest req,
+            CancellationToken cancellationToken) =>
+            await req.UserRequest<ConductRequestDto>(this.functionAuthenticator, async (user, payload) =>
+            {
+                if (string.IsNullOrWhiteSpace(payload.DeviceId) ||
+                    string.IsNullOrWhiteSpace(payload.ChannelName) ||
+                    string.IsNullOrWhiteSpace(payload.ContactName))
+                    throw new ExpectedHttpException(
+                        HttpStatusCode.BadRequest,
+                        "DeviceId, ChannelName and ContactName properties are required.");
+                
+                // TODO: Queue conduct
+            }, cancellationToken);
+
+        private class ConductRequestDto
+        {
+            public string? DeviceId { get; set; }
+
+            public string? ChannelName { get; set; }
+            
+            public string? ContactName { get; set; }
+
+            public string? ValueSerialized { get; set; }
+        }
+    }
+}
