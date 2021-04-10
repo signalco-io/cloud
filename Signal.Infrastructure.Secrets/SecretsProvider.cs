7b71ed268d73494ef3af97e3549e5b9f2baa4411
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-04-10 22:05:11 +0200

    KeyVault secrets now staticly cached

--- Signal.Infrastructure.Secrets/SecretsProvider.cs
+++ Signal.Infrastructure.Secrets/SecretsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Identity;
@@ -11,8 +12,19 @@ namespace Signal.Infrastructure.Secrets
     public class SecretsProvider : ISecretsProvider
     {
         private readonly IConfiguration configuration;
+
         // TODO: Move to configuration
         private const string KeyVaultUrl = "https://signal.vault.azure.net/";
+
+        private static readonly SecretClient Client;
+        private static readonly Dictionary<string, string> Cache = new Dictionary<string, string>();
+
+        static SecretsProvider()
+        {
+            Client = new SecretClient(
+                new Uri(KeyVaultUrl),
+                new DefaultAzureCredential());
+        }
 
         public SecretsProvider(IConfiguration configuration)
         {
@@ -30,11 +42,11 @@ namespace Signal.Infrastructure.Secrets
                 // Shit, try in vault
             }
 
-            var client = new SecretClient(
-                new Uri(KeyVaultUrl),
-                new DefaultAzureCredential());
-            var secret = await client.GetSecretAsync(key, cancellationToken: cancellationToken);
-            return secret.Value.Value;
+            if (Cache.ContainsKey(key)) 
+                return Cache[key];
+
+            var secret = await Client.GetSecretAsync(key, cancellationToken: cancellationToken);
+            return Cache[key] = secret.Value.Value;
         }
     }
 }
