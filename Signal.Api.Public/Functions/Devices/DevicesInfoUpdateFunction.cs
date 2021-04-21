d1bc75add980a3184dee44f6c22507b70c18ae6f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-04-21 18:24:56 +0200

    Device infor update endpoint

--- Signal.Api.Public/Functions/Devices/DevicesInfoUpdateFunction.cs
+++ Signal.Api.Public/Functions/Devices/DevicesInfoUpdateFunction.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -10,7 +8,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Signal.Api.Public.Auth;
 using Signal.Api.Public.Exceptions;
-using Signal.Api.Public.Functions.Devices.Dtos;
 using Signal.Core;
 using Signal.Core.Devices;
 using Signal.Core.Exceptions;
@@ -18,13 +15,13 @@ using Signal.Core.Storage;
 
 namespace Signal.Api.Public.Functions.Devices
 {
-    public class DevicesEndpointsUpdateFunction
+    public class DevicesInfoUpdateFunction
     {
         private readonly IFunctionAuthenticator functionAuthenticator;
         private readonly IAzureStorage storage;
         private readonly IAzureStorageDao storageDao;
 
-        public DevicesEndpointsUpdateFunction(
+        public DevicesInfoUpdateFunction(
             IFunctionAuthenticator functionAuthenticator,
             IAzureStorage storage,
             IAzureStorageDao storageDao)
@@ -35,37 +32,46 @@ namespace Signal.Api.Public.Functions.Devices
             this.storageDao = storageDao ?? throw new ArgumentNullException(nameof(storageDao));
         }
 
-        [FunctionName("Devices-EndpointsUpdate")]
+        [FunctionName("Devices-InfoUpdate")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "devices/endpoints/update")]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "devices/info/update")]
             HttpRequest req,
             CancellationToken cancellationToken) =>
-            await req.UserRequest<DeviceEndpointsUpdateDto>(this.functionAuthenticator, async (user, payload) =>
+            await req.UserRequest<DeviceInfoUpdateDto>(this.functionAuthenticator, async (user, payload) =>
             {
                 if (string.IsNullOrWhiteSpace(payload.DeviceId))
                     throw new ExpectedHttpException(
                         HttpStatusCode.BadRequest,
-                        "DeviceIdentifier property is required.");
+                        "DeviceId property is required.");
+                if (string.IsNullOrWhiteSpace(payload.Alias))
+                    throw new ExpectedHttpException(
+                        HttpStatusCode.BadRequest,
+                        "Alias property is required.");
 
                 // Check if user has assigned requested device
-                if (!await this.storageDao.IsUserAssignedAsync(user.UserId, TableEntityType.Device, payload.DeviceId,
-                    cancellationToken))
+                if (!await this.storageDao.IsUserAssignedAsync(user.UserId, TableEntityType.Device, payload.DeviceId, cancellationToken))
                     throw new ExpectedHttpException(HttpStatusCode.NotFound);
-                
+
                 // Commit endpoints
                 await this.storage.CreateOrUpdateItemAsync(
                     ItemTableNames.Devices,
-                    new DeviceTableEndpointsEntity(
+                    new DeviceInfoTableEntity(
                         payload.DeviceId,
-                        JsonSerializer.Serialize(payload.Endpoints)),
+                        payload.Alias, 
+                        payload.Manufacturer,
+                        payload.Model),
                     cancellationToken);
             }, cancellationToken);
 
-        private class DeviceEndpointsUpdateDto
+        private class DeviceInfoUpdateDto
         {
             public string? DeviceId { get; set; }
 
-            public IEnumerable<DeviceEndpointDto> Endpoints { get; set; }
+            public string? Alias { get; set; }
+
+            public string? Manufacturer { get; set; }
+
+            public string? Model { get; set; }
         }
     }
 }
---
+++ Signal.Core/Devices/DeviceInfoTableEntity.cs
@@ -0,0 +1,26 @@
+﻿using Signal.Core.Storage;
+
+namespace Signal.Core.Devices
+{
+    public class DeviceInfoTableEntity : ITableEntity
+    {
+        public DeviceInfoTableEntity(string deviceId, string alias, string? manufacturer, string? model)
+        {
+            this.PartitionKey = "device";
+            this.RowKey = deviceId;
+            this.Alias = alias;
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+        }
+
+        public string Alias { get; set; }
+
+        public string? Manufacturer { get; set; }
+
+        public string? Model { get; set; }
+        
+        public string PartitionKey { get; }
+
+        public string RowKey { get; }
+    }
+}
