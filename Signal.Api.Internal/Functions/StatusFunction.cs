681e65ed31fb6b96d6663f47fe1d9dcdadab88cf
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-11-22 22:29:17 +0100

    Added status functions for public and internal
    Issue https://github.com/signalco-io/signalco/issues/134

---
+++ Signal.Api.Internal/Functions/StatusFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace Signal.Api.Internal.Functions;
+
+public class StatusFunction
+{
+    [FunctionName("StatusFunction")]
+    [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+    [OpenApiResponseWithoutBody(HttpStatusCode.OK, Description = "API status is running.")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest _) =>
+        new OkResult();
+}
