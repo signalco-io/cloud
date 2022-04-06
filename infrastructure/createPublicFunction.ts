ca321f9c6744eab3d4ed53c38712eec0a0b062a3
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-04-06 12:24:55 +0200

    Interpolate full domain name

--- infrastructure/createPublicFunction.ts
+++ infrastructure/createPublicFunction.ts
@@ -1,11 +1,11 @@
 import { ResourceGroup } from '@pulumi/azure-native/resources';
-import { Config } from '@pulumi/pulumi';
+import { interpolate, Config } from '@pulumi/pulumi';
 import { assignCustomDomain } from './assignCustomDomain';
 import { createFunction } from './createFunction';
 
 export default function createPublicFunction (resourceGroup: ResourceGroup, namePrefix: string, subDomainName: string, protect: boolean) {
     const config = new Config();
-    const fullDomainName = `${subDomainName}.${config.require('domain')}`;
+    const fullDomainName = interpolate`${subDomainName}.${config.require('domain')}`;
     const pubFunc = createFunction(resourceGroup, namePrefix, protect, fullDomainName);
     const domain = assignCustomDomain(resourceGroup, pubFunc.webApp, pubFunc.servicePlan, namePrefix, subDomainName, protect);
 
