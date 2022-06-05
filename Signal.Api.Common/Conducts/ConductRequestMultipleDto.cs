55f333ba5407d065942feef6ed5d64ab0237ec48
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-06-05 19:07:58 +0200

    Update ConductRequestMultipleDto.cs

--- Signal.Api.Common/Conducts/ConductRequestMultipleDto.cs
+++ Signal.Api.Common/Conducts/ConductRequestMultipleDto.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Signal.Api.Common.Conducts
 {
     [Serializable]
     public class ConductRequestMultipleDto
     {
+        [JsonPropertyName("deviceId")]
         [Required]
         public string? DeviceId { get; set; }
 
+        [JsonPropertyName("channelName")]
         [Required]
         public string? ChannelName { get; set; }
 
+        [JsonPropertyName("contactName")]
         [Required]
         public string? ContactName { get; set; }
 
+        [JsonPropertyName("valueSerialized")]
         public string? ValueSerialized { get; set; }
 
+        [JsonPropertyName("delay")]
         public double? Delay { get; set; }
     }
 }
